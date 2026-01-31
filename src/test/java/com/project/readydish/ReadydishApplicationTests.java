b3083bcabeace080ac3278df0051afb5f20a9780
Davi Clemente <cldavifs@gmail.com>
2026-01-31 01:27:52 -0300

    Estrutura do projeto

---
+++ src/test/java/com/project/readydish/ReadydishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.readydish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadydishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
